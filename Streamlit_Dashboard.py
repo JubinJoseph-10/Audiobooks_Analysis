840bae48e9ef0779749f8bec9659a686c2a292de
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-03-30 18:23:21 +0530

    Update Streamlit_Dashboard.py

--- Streamlit_Dashboard.py
+++ Streamlit_Dashboard.py
@@ -27,13 +27,13 @@ req_data = pd.read_csv("Data/Audible_Dashboard_Data.csv")
 
 #structuring the space for the analysis
 demo_listening_habs = st.container(border=True)
-demo_listening_habs.markdown('<div style="text-align: center; font-size: 24px">Impact of Demographics on User Habbits</div>',unsafe_allow_html=True)
+demo_listening_habs.markdown('<div style="text-align: center; font-size: 24px">Impact of Demographics on User Habbits & Preferences</div>',unsafe_allow_html=True)
 demo_listening_habs.write('\n')
 demo_listening_habs_chart_space ,demo_listening_habs_des = demo_listening_habs.columns([.7,.3])
 demo_listening_habs_des_ = demo_listening_habs_des.container(border =True)
 demo_listening_habs_chart_space_ = demo_listening_habs_chart_space.container(border =True)
 
-demo_listening_habs_des_.markdown('<div style="text-align: justify; font-size: 18px"></div>',unsafe_allow_html=True)
+demo_listening_habs_des_.markdown('<div style="text-align: justify; font-size: 18px">User Habbits</div>',unsafe_allow_html=True)
 demo_listening_habs_des_.markdown('<div style="text-align: justify; font-size: 18px"></div>',unsafe_allow_html=True)
 
 
@@ -58,14 +58,13 @@ demo_listening_habs_chart_space_.plotly_chart(habbit_chart)
 ##### Habbits Across Users 
 
 #structuring the space for the analysis
-demo_listening_pref = st.container(border=True)
 demo_listening_pref_des , demo_listening_pref_chart_space = demo_listening_habs.columns([.3,.7])
 demo_listening_pref_des_ = demo_listening_pref_des.container(border =True)
 demo_listening_pref_chart_space_ = demo_listening_pref_chart_space.container(border =True)
 
 
-demo_listening_pref_des_.markdown('<div style="text-align: justify; font-size: 24px">Impact of Demographics on User Preferences</div>',unsafe_allow_html=True)
-demo_listening_pref_des_.markdown('<div style="text-align: justify; font-size: 16px"></div>',unsafe_allow_html=True)
+demo_listening_pref_des_.markdown('<div style="text-align: justify; font-size: 18px">User Preferences</div>',unsafe_allow_html=True)
+demo_listening_pref_des_.markdown('<div style="text-align: justify; font-size: 18px"></div>',unsafe_allow_html=True)
 
 
 
