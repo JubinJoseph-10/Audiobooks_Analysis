56dc962458683a93fbd06e352262fa2fa4b581cd
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-04-03 12:43:06 +0530

    Update Streamlit_Dashboard.py

--- Streamlit_Dashboard.py
+++ Streamlit_Dashboard.py
@@ -115,13 +115,13 @@ top_book = data_top_audiobooks.iloc[0,0]
 top_narrator = data_top_authors.iloc[0,0]
 top_genre = data_top_genre.iloc[0,0]
 top_pub = data_top_pub.iloc[0,0]
-further_.markdown('<div style="text-align: justify; font-size: 14px">1. The most heard book in {} with {} as {} based on {} is {}.</div>'.format(state_name,demograph,demograph_specifc,basis_top_audiobooks,top_narrator),unsafe_allow_html=True) 
+further_.markdown('<div style="text-align: justify; font-size: 14px">1. The most heard book in {} with {} as {} based on {} is **{}**.</div>'.format(state_name,demograph,demograph_specifc,basis_top_audiobooks,top_book),unsafe_allow_html=True) 
 further_.write('\n')
-further_.markdown('<div style="text-align: justify; font-size: 14px">1. The most most heard Narrator in {} with {} as {} based on {} is {}.</div>'.format(state_name,demograph,demograph_specifc,basis_top_author,top_book),unsafe_allow_html=True) 
+further_.markdown('<div style="text-align: justify; font-size: 14px">2. The most most heard Narrator in {} with {} as {} based on {} is **{}**.</div>'.format(state_name,demograph,demograph_specifc,basis_top_author,top_narrator),unsafe_allow_html=True) 
 further_.write('\n')
-further_.markdown('<div style="text-align: justify; font-size: 14px">1. The most heard genre in {} with {} as {} based on {} is {}.</div>'.format(state_name,demograph,demograph_specifc,basis_top_genre,top_genre),unsafe_allow_html=True) 
+further_.markdown('<div style="text-align: justify; font-size: 14px">3. The most heard genre in {} with {} as {} based on {} is **{}**.</div>'.format(state_name,demograph,demograph_specifc,basis_top_genre,top_genre),unsafe_allow_html=True) 
 further_.write('\n')
-further_.markdown('<div style="text-align: justify; font-size: 14px">1. The most prominent publisher in {} with {} as {} based on {} is {}.</div>'.format(state_name,demograph,demograph_specifc,basis_top_pub,top_pub),unsafe_allow_html=True) 
+further_.markdown('<div style="text-align: justify; font-size: 14px">4. The most prominent publisher in {} with {} as {} based on {} is **{}**.</div>'.format(state_name,demograph,demograph_specifc,basis_top_pub,top_pub),unsafe_allow_html=True) 
 further_.write('\n')
 
 
