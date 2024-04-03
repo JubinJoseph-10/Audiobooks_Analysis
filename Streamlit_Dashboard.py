fe78b2e26d15fabdbd45d62423c029693e92c551
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-04-03 10:58:58 +0530

    Update Streamlit_Dashboard.py

--- Streamlit_Dashboard.py
+++ Streamlit_Dashboard.py
@@ -59,34 +59,28 @@ def basis_data_sorter(nature,base):
 
 
 ###Top audiobooks section
-further_top_audiobooks = further_.container(border=True)
-further_top_audiobooks_1 , further_top_audiobooks_2 = further_top_audiobooks.columns([.3,.7])
-further_top_audiobooks_1_ = further_top_audiobooks_1.container(border=True)
-further_top_audiobooks_2_ = further_top_audiobooks_2.container(border=True)
-basis_top_audiobooks = further_top_audiobooks_1_.selectbox('Select a Basis for Choosing Top Authors',['Number of Listeners','Number of Reviews','Best Ratings','Most Shared'],key=89)
+
+graphs = top_section.container(border=True)
+further_top_audiobooks,further_top_narrators,further_top_genre,further_top_publishers = graphs.columns([.25,.25,.25,.25])
+
+
+further_top_audiobooks_ = further_top_audiobooks.container(border=True)
+basis_top_audiobooks = further_top_audiobooks_.selectbox('Select a Basis for Choosing Top Authors',['Number of Listeners','Number of Reviews','Best Ratings','Most Shared'],key=89)
 data_top_audiobooks = basis_data_sorter('Product_Name',basis_top_audiobooks)
 top_audio_chart = px.bar(data_top_audiobooks,x='Product_Name',barmode='group',color='Product_Name',
                         y=data_top_audiobooks.columns[1],title=f'Top Audiobooks based on {data_top_audiobooks.columns[1]}',height=400,width=450)
 top_audio_chart.update_layout(showlegend=False)
 top_audio_chart.update_xaxes(showticklabels=False, title='')
-further_top_audiobooks_2_.plotly_chart(top_audio_chart,use_column_width=True)
+further_top_audiobooks_.plotly_chart(top_audio_chart,use_column_width=True)
 
 
 ###
-further_top_author = further.container(border=True)
-further_top_author_1 , further_top_author_2 = further_top_author.columns([.7,.3])
-further_top_author_1_ = further_top_author_1.container(border=True)
-further_top_author_2_ = further_top_author_2.container(border=True)
-basis_top_author = further_top_author_2_.selectbox('Select a Basis for Choosing Top Authors',['Number of Listeners','Number of Reviews','Average Ratings','Social Sharing'],key =49)
+further_top_narrators_ = further_top_narrators.container(border=True)
+basis_top_author = further_top_narrators_.selectbox('Select a Basis for Choosing Top Authors',['Number of Listeners','Number of Reviews','Average Ratings','Social Sharing'],key =49)
 data_top_authors = basis_data_sorter('Product_Narrator',basis_top_audiobooks)
 top_author_chart = px.bar(data_top_authors,x='Product_Narrator',barmode='group',color='Product_Narrator',
                         y=data_top_authors.columns[1],title=f'Top Audiobooks based on {data_top_authors.columns[1]}',height=400,width=450)
-further_top_author_1_.plotly_chart(top_author_chart,use_column_width=True)
-
-
-further_top_narrators = further.container(border=True)
-
-
+further_top_narrators_.plotly_chart(top_author_chart,use_column_width=True)
 
 
 further_top_genre = further.container(border=True)
