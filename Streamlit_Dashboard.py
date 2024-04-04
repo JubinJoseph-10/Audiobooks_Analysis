ab9466b9921aee49d4a3a68d4d6702f669a8e0d5
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-04-04 11:21:27 +0530

    Update Streamlit_Dashboard.py

--- Streamlit_Dashboard.py
+++ Streamlit_Dashboard.py
@@ -622,7 +622,7 @@ def sel_feat_returner():
     X_Scaled = scaler.fit_transform(model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1))
     X, y = smt.fit_resample(X_Scaled,req_data['Completion_Rate_5_Weeks'].apply(lambda x: 1 if x>=100 else 0))
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
-    sel = SelectFromModel(RandomForestClassifier(),threshold=0.1)
+    sel = SelectFromModel(RandomForestClassifier())
     sel.fit(X_train, y_train)
     sel.get_support()
     selected_feat = model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1).columns[sel.get_support()]
@@ -894,25 +894,5 @@ fig.update_layout(
 )
 
 model_social_sharing_chart_.plotly_chart(fig,use_container_width=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
