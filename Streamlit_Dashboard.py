bd84deca317c2167802380b6a83bd51e58bac798
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-04-04 11:22:25 +0530

    Update Streamlit_Dashboard.py

--- Streamlit_Dashboard.py
+++ Streamlit_Dashboard.py
@@ -704,7 +704,7 @@ def sel_feat_returner():
     X_Scaled = scaler.fit_transform(model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1))
     X, y = smt.fit_resample(X_Scaled,req_data['Ratings_Given'].apply(lambda x:rating_sorter(x)))
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
-    sel = SelectFromModel(RandomForestClassifier(),threshold=0.1)
+    sel = SelectFromModel(RandomForestClassifier())
     sel.fit(X_train, y_train)
     sel.get_support()
     selected_feat =  model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1).columns[sel.get_support()]
@@ -773,7 +773,7 @@ def sel_feat_returner():
     X_Scaled = scaler.fit_transform(model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1))
     X, y = smt.fit_resample(X_Scaled,req_data['Recommendations_Followed'])
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
-    sel = SelectFromModel(RandomForestClassifier(),threshold=0.1)
+    sel = SelectFromModel(RandomForestClassifier())
     sel.fit(X_train, y_train)
     sel.get_support()
     selected_feat = model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1).columns[(sel.get_support())]
@@ -843,7 +843,7 @@ def sel_feat_returner():
     X_Scaled = scaler.fit_transform(model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1))
     X, y = smt.fit_resample(X_Scaled,req_data['Social_Sharing'])
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)  
-    sel = SelectFromModel(RandomForestClassifier(),threshold=0.1)
+    sel = SelectFromModel(RandomForestClassifier())
     sel.fit(X_train, y_train)
     sel.get_support()
     selected_feat = model_data.drop(['Completion_Rate_2_Weeks','Completion_Rate_5_Weeks','Social_Sharing','Ratings_Given','Recommendations_Followed'],axis=1).columns[(sel.get_support())]
